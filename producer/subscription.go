212dcdfd29f7f5b835f918a79beeaecc2e5ef0d9
s123tg <56829688+s123tg@users.noreply.github.com>
2021-08-29 23:36:16 +0800

    Update subscription.go

--- producer/subscription.go
+++ producer/subscription.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 	"reflect"
 
-	"github.com/free5gc/amf/context"
-	"github.com/free5gc/amf/logger"
+	"github.com/s123tg/amf/context"
+	"github.com/s123tg/amf/logger"
 	"github.com/free5gc/http_wrapper"
 	"github.com/free5gc/openapi/models"
 )
