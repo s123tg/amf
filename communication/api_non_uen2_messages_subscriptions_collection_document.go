c533f14da64f2baf113696a584bf44d88d934236
s123tg <56829688+s123tg@users.noreply.github.com>
2021-08-29 23:18:48 +0800

    Update api_non_uen2_messages_subscriptions_collection_document.go

--- communication/api_non_uen2_messages_subscriptions_collection_document.go
+++ communication/api_non_uen2_messages_subscriptions_collection_document.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/free5gc/amf/logger"
+	"github.com/s123tg/amf/logger"
 )
 
 // NonUeN2InfoSubscribe - Namf_Communication Non UE N2 Info Subscribe service Operation
