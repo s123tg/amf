b5fff0a00ef6d372718d36ba8d9aab1018728dff
s123tg <56829688+s123tg@users.noreply.github.com>
2021-08-29 23:34:43 +0800

    Update n1n2message.go

--- producer/callback/n1n2message.go
+++ producer/callback/n1n2message.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	amf_context "github.com/free5gc/amf/context"
-	"github.com/free5gc/amf/logger"
+	amf_context "github.com/s123tg/amf/context"
+	"github.com/s123tg/amf/logger"
 	"github.com/free5gc/openapi/Namf_Communication"
 	"github.com/free5gc/openapi/models"
 )
