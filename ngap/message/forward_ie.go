af15b4be36e73f75d60c6f7170f21ea25f59e28b
s123tg <56829688+s123tg@users.noreply.github.com>
2021-08-29 23:31:13 +0800

    Update forward_ie.go

--- ngap/message/forward_ie.go
+++ ngap/message/forward_ie.go
@@ -3,8 +3,8 @@ package message
 import (
 	"encoding/hex"
 
-	"github.com/free5gc/amf/context"
-	"github.com/free5gc/amf/logger"
+	"github.com/s123tg/amf/context"
+	"github.com/s123tg/amf/logger"
 	"github.com/free5gc/ngap/ngapConvert"
 	"github.com/free5gc/ngap/ngapType"
 	"github.com/free5gc/openapi/models"
